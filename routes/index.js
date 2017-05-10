919c6442d32eb9394b2d7c7115bf73e35dd143a4
dec0mpiled <liimbix@gmail.com>
2017-05-10 13:42:15 +0000

    three

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'zOOfis', fetch:fetch });
+  res.render('index', { title: 'zOOfis'});
 });
 
 module.exports = router;
